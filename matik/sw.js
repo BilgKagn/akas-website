f237652bb6a9baa39c5e77762f94b5de54b4984a
BilgKagn <baki@akasmuhendislik.com.tr>
2026-05-05 00:03:37 +0300

    Add files via upload

---
+++ matik/sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'matik-v1';
+const FILES = [
+  '/matik/',
+  '/matik/index.html',
+  '/matik/manifest.json',
+  '/matik/math_icon_192.png',
+  '/matik/math_icon_512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
